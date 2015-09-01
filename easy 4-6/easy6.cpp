47ab96a5345c4dd3c14f061497f1993a0dda34df
hellozts4120 <14307110190@fudan.edu.cn>
2015-09-01 21:43:10 +0800

    quite difficult to get the answer of operator [].

---
+++ easy 4-6/easy6.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Array2{
+private:
+	int *p;
+	int r,c;
+public:
+	Array2(){p = NULL;}
+	Array2(int r_, int c_){
+		r = r_;
+		c = c_;
+		p = new int[r * c];
+	}
+	Array2(const Array2 & arr2){
+		r = arr2.r;
+		c = arr2.c;
+		p = new int[r * c];
+		memcpy(p,arr2.p,sizeof(int) * (r * c));
+	}			//复制构造函数可写可不写
+	Array2 & operator=(const Array2 & arr2){
+		if(p){
+			delete []p;
+		}
+		r = arr2.r;
+		c = arr2.c;
+		p = new int[r * c];
+		memcpy(p,arr2.p,sizeof(int) * (r * c));
+		return *this;
+	}
+	~Array2(){
+		if(p)
+			delete []p;
+	}
+	int * operator[](int i){
+		return p + i * c;	//此处重点
+	}
+	int & operator()(int i,int j){
+		return p[i * c + j];
+	}
+};
+	
+
+	int main() {
+    Array2 a(3,4);
+    int i,j;
+    for(  i = 0;i < 3; ++i )
+        for(  j = 0; j < 4; j ++ )
+            a[i][j] = i * 4 + j;
+    for(  i = 0;i < 3; ++i ) {
+        for(  j = 0; j < 4; j ++ ) {
+            cout << a(i,j) << ",";
+        }
+        cout << endl;
+    }
+    cout << "next" << endl;
+    Array2 b;     b = a;
+    for(  i = 0;i < 3; ++i ) {
+        for(  j = 0; j < 4; j ++ ) {
+            cout << b[i][j] << ",";
+        }
+        cout << endl;
+    }
+    return 0;
+}
